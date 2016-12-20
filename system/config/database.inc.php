dc0abe92ef2f29e7592cb9476ce0b89436d15006
PAPA <mailpapa@qq.com>
2016-12-21 06:00:18 +0800

    Update database.inc.php

--- system/config/database.inc.php
+++ system/config/database.inc.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 'default' => array (
-'hostname' => 'localhost','database' => 'ceshi','username' => 'root','password' => '123456','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+'hostname' => '192.168.1.91:30032','database' => '9c13358fe32f4','username' => 'a9a2f4cde5594','password' => '5e5aea3797914','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
 );
